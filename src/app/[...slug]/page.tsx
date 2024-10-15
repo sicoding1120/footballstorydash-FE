69c881768bd9abe8abe770bf6134c1cef81c5427
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-10-15 14:18:05 +0700

    update ulang

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -16,7 +16,7 @@ const DynamicRoot = () => {
   useEffect(() => {
     const handleReceiveMessage = (event: MessageEvent) => {
       // Pastikan pesan berasal dari https://footballstory.vercel.app
-      if (event.origin === "https://footballstory.vercel.app") {
+      if (event.origin === "https://footballstorydash.vercel.app") {
         const receivedToken = event.data;
         console.log("Token diterima:", receivedToken);
         setToken(event);
