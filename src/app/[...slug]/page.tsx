c8998e0898e3998dfb6033471953aa53d8318e2b
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-10-15 13:00:00 +0700

    update

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,21 +1,35 @@
-'use client'
+"use client";
 
-import { fetcher } from '@/lib/fetcher';
-import { useParams } from 'next/navigation'
-import React, { useEffect } from 'react'
-import useSWR from 'swr';
-
+import { fetcher } from "@/lib/fetcher";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
+import useSWR from "swr";
 
 const DynamicRoot = () => {
   const { slug } = useParams();
-  const { data, error, isLoading } = useSWR(`https://footballstorybe.vercel.app/user/${slug.at(1)}`, fetcher);
-  console.log(data, error, isLoading);  
-  return (
-    <div>Data user: 
-      <p>{data.data.username}</p>
-      <p>{data.data.email}</p>
-    </div>
-  )
-}
+  const { data, error, isLoading } = useSWR(
+    `https://footballstorybe.vercel.app/user/${slug.at(1)}`,
+    fetcher
+  );
 
-export default DynamicRoot
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      if (event.origin === "https://footballstory.vercel.app/") {
+        const token = event.data;
+        console.log("Access Token Received: ", token);
+      }
+    });
+  }, []);
+
+  
+  console.log(data, error, isLoading);
+  return (
+    <div>
+      Data user:
+      <p>{data?.data?.username}</p>
+      <p>{data?.data?.email}</p>
+    </div>
+  );
+};
+
+export default DynamicRoot;
